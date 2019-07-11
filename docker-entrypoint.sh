2958221d64ed2419dec5d7404759d985f247ca13
Vladislav Yarmak <vladislav@vm-0.com>
2019-07-11 03:31:01 +0300

    docker-entrypoint: ensure output dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "/patched-lib" > /etc/ld.so.conf.d/000-patched-lib.conf && \
+mkdir -p "/patched-lib" && \
 PATCH_OUTPUT_DIR=/patched-lib /usr/local/bin/patch.sh && \
 cd /patched-lib && \
 for f in * ; do
