b3d131139709e878f8a901f13fd7be38dec5119e
Vladislav Yarmak <vladislav@vm-0.com>
2019-12-03 13:27:46 +0200

    win: fbc: 32bit compat code

--- win/nvfbcwrp/nvfbcbody.asm
+++ win/nvfbcwrp/nvfbcbody.asm
@@ -1,37 +1,43 @@
+IFDEF RAX
+ptrsz equ <qword>
+ELSE
+.486
+.model flat, c
+ptrsz equ <dword>
+ENDIF
 .data
-
-extern ORIG_NvFBC_Create : qword, ORIG_NvFBC_Enable : qword,
-	ORIG_NvFBC_GetSDKVersion : qword, ORIG_NvFBC_GetStatus : qword,
-	ORIG_NvFBC_GetStatusEx : qword, ORIG_NvFBC_SetGlobalFlags : qword,
-	ORIG_NvOptimusEnablement : qword
+extern ORIG_NvFBC_Create : ptrsz, ORIG_NvFBC_Enable : ptrsz,
+	ORIG_NvFBC_GetSDKVersion : ptrsz, ORIG_NvFBC_GetStatus : ptrsz,
+	ORIG_NvFBC_GetStatusEx : ptrsz, ORIG_NvFBC_SetGlobalFlags : ptrsz,
+	ORIG_NvOptimusEnablement : ptrsz
 
 .code
 PROXY_NvFBC_Create proc
-jmp qword ptr [ORIG_NvFBC_Create]
+jmp ptrsz ptr [ORIG_NvFBC_Create]
 PROXY_NvFBC_Create endp
 
 PROXY_NvFBC_Enable proc
-jmp qword ptr [ORIG_NvFBC_Enable]
+jmp ptrsz ptr [ORIG_NvFBC_Enable]
 PROXY_NvFBC_Enable endp
 
 PROXY_NvFBC_GetSDKVersion proc
-jmp qword ptr [ORIG_NvFBC_GetSDKVersion]
+jmp ptrsz ptr [ORIG_NvFBC_GetSDKVersion]
 PROXY_NvFBC_GetSDKVersion endp
 
 PROXY_NvFBC_GetStatus proc
-jmp qword ptr [ORIG_NvFBC_GetStatus]
+jmp ptrsz ptr [ORIG_NvFBC_GetStatus]
 PROXY_NvFBC_GetStatus endp
 
 PROXY_NvFBC_GetStatusEx proc
-jmp qword ptr [ORIG_NvFBC_GetStatusEx]
+jmp ptrsz ptr [ORIG_NvFBC_GetStatusEx]
 PROXY_NvFBC_GetStatusEx endp
 
 PROXY_NvFBC_SetGlobalFlags proc
-jmp qword ptr [ORIG_NvFBC_SetGlobalFlags]
+jmp ptrsz ptr [ORIG_NvFBC_SetGlobalFlags]
 PROXY_NvFBC_SetGlobalFlags endp
 
 PROXY_NvOptimusEnablement proc
-jmp qword ptr [ORIG_NvOptimusEnablement]
+jmp ptrsz ptr [ORIG_NvOptimusEnablement]
 PROXY_NvOptimusEnablement endp
 
 end
