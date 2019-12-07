43fe5419614496855b8572d4eed04bc15ddc5710
Vladislav Yarmak <vladislav@vm-0.com>
2019-12-07 23:41:30 +0200

    win: nvfbcwrp: add sniffing and custom data spoofing code

--- win/nvfbcwrp/framework.h
+++ win/nvfbcwrp/framework.h
@@ -3,3 +3,5 @@
 #define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
 // Файлы заголовков Windows
 #include <windows.h>
+#include <new>
+#include <tuple>
