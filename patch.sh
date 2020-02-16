e9a459d68fa134024b9b601cc63c6709c087cc91
Vladislav Yarmak <vladislav@vm-0.com>
2020-02-16 11:13:30 +0200

    linux driver 440.58.01

--- patch.sh
+++ patch.sh
@@ -87,6 +87,7 @@ declare -A patch_list=(
     ["440.43.01"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
     ["440.44"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
     ["440.48.02"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
+    ["440.58.01"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
     ["440.59"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
 )
 
@@ -133,6 +134,7 @@ declare -A object_list=(
     ["440.43.01"]='libnvidia-encode.so'
     ["440.44"]='libnvidia-encode.so'
     ["440.48.02"]='libnvidia-encode.so'
+    ["440.58.01"]='libnvidia-encode.so'
     ["440.59"]='libnvidia-encode.so'
 )
 
