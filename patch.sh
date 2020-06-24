a1fcbee06ba749c82297a15e85099e95f49d019d
Vladislav Yarmak <vladislav@vm-0.com>
2020-06-24 20:33:38 +0300

    linux: add driver version 450.51

--- patch.sh
+++ patch.sh
@@ -106,6 +106,7 @@ declare -A patch_list=(
     ["440.95.01"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
     ["440.82"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
     ["450.36.06"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
+    ["450.51"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
 )
 
 declare -A object_list=(
@@ -170,6 +171,7 @@ declare -A object_list=(
     ["440.95.01"]='libnvidia-encode.so'
     ["440.82"]='libnvidia-encode.so'
     ["450.36.06"]='libnvidia-encode.so'
+    ["450.51"]='libnvidia-encode.so'
 )
 
 check_version_supported () {
