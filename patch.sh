d5b06556c91bf390c52632d6ce11df59a53a9526
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2020-11-24 23:34:08 +0200

    linux: add vulkan beta driver version 455.46.02 (NVENC only)

--- patch.sh
+++ patch.sh
@@ -131,6 +131,7 @@ declare -A patch_list=(
     ["455.38"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
     ["455.45.01"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
     ["455.46.01"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
+    ["455.46.02"]='s/\x85\xc0\x41\x89\xc4\x75\x1f/\x31\xc0\x41\x89\xc4\x75\x1f/g'
 )
 
 declare -A object_list=(
@@ -217,6 +218,7 @@ declare -A object_list=(
     ["455.38"]='libnvidia-encode.so'
     ["455.45.01"]='libnvidia-encode.so'
     ["455.46.01"]='libnvidia-encode.so'
+    ["455.46.02"]='libnvidia-encode.so'
 )
 
 check_version_supported () {
