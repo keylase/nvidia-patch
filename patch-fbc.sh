069a0d63da3ef6c342aa62ff0f165e978d241fab
JustCryen <mcgranat999@gmail.com>
2021-12-14 16:02:30 +0100

    Added support for 495.46

--- patch-fbc.sh
+++ patch-fbc.sh
@@ -133,6 +133,7 @@ declare -A patch_list=(
     ["470.86"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
     ["495.29.05"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
     ["495.44"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
+    ["495.46"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
 )
 
 declare -A object_list=(
@@ -214,6 +215,7 @@ declare -A object_list=(
     ["470.86"]='libnvidia-fbc.so'
     ["495.29.05"]='libnvidia-fbc.so'
     ["495.44"]='libnvidia-fbc.so'
+    ["495.46"]='libnvidia-fbc.so'
 )
 
 check_version_supported () {
