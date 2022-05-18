7636605917b25c3abb52c4a44277ba4380782525
Jai Luthra <me@jailuthra.in>
2022-05-18 21:31:38 +0530

    linux: nvenc patch for 510.73.05

--- patch-fbc.sh
+++ patch-fbc.sh
@@ -143,6 +143,7 @@ declare -A patch_list=(
     ["510.54"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
     ["510.60.02"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
     ["510.68.02"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
+    ["510.73.05"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
     ["515.43.04"]='s/\x83\xfe\x01\x73\x08\x48/\x83\xfe\x00\x72\x08\x48/'
 )
 
@@ -235,6 +236,7 @@ declare -A object_list=(
     ["510.54"]='libnvidia-fbc.so'
     ["510.60.02"]='libnvidia-fbc.so'
     ["510.68.02"]='libnvidia-fbc.so'
+    ["510.73.05"]='libnvidia-fbc.so'
     ["515.43.04"]='libnvidia-fbc.so'
 )
 
