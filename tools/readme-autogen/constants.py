b1ddf47d5ebc0f4c015746433bff41eefc72499b
reloxx13 <reloxx@interia.pl>
2022-05-24 23:50:31 +0200

    support dch driver variant

--- tools/readme-autogen/constants.py
+++ tools/readme-autogen/constants.py
@@ -39,6 +39,7 @@ ENCODING='utf-8'
 DRIVER_URL_TEMPLATE = {
     (OSKind.Linux, None, None, None):                                       "https://international.download.nvidia.com/XFree86/Linux-x86_64/$version/NVIDIA-Linux-x86_64-$version.run",
     (OSKind.Windows, Product.GeForce, WinSeries.win10, ''):                 "https://international.download.nvidia.com/Windows/$version/$version-desktop-win10-win11-64bit-international-whql.exe",
+    (OSKind.Windows, Product.GeForce, WinSeries.win10, 'DCH'):              "https://international.download.nvidia.com/Windows/$version/$version-desktop-win10-win11-64bit-international-dch-whql.exe",
     (OSKind.Windows, Product.GeForce, WinSeries.win10, 'Studio Driver'):    "https://international.download.nvidia.com/Windows/$version/$version-desktop-win10-win11-64bit-international-nsd-whql.exe",
     (OSKind.Windows, Product.GeForce, WinSeries.win10, 'Vulkan Beta'):      "",
     (OSKind.Windows, Product.Quadro, WinSeries.win10, ''):                  "https://international.download.nvidia.com/Windows/Quadro_Certified/$version/$version-quadro-rtx-desktop-notebook-win10-win11-64bit-international-whql.exe",
@@ -51,6 +52,7 @@ DRIVER_URL_TEMPLATE = {
 
 DRIVER_DIR_PREFIX = {
     (Product.GeForce, ''): '',
+    (Product.GeForce, 'DCH'): '',
     (Product.GeForce, 'Studio Driver'): 'nsd_',
     (Product.GeForce, 'Vulkan Beta'): '',
     (Product.Quadro, ''): 'quadro_',
