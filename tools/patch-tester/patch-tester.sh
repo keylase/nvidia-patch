ac0afeb01057ce02afec07d761d7374ce6260a79
siriume <siriume@gmail.com>
2024-10-26 22:42:01 +0800

    update_patch_tester

--- tools/patch-tester/patch-tester.sh
+++ tools/patch-tester/patch-tester.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -f lavfi -i testsrc -t 50 -vf hwupload -c:a copy -c:v h264_nvenc -b:v 4M -f null - -vf hwupload -c:a copy -c:v h264_nvenc -b:v 1M -f null - -vf hwupload -c:a copy -c:v h264_nvenc -b:v 8M -f null - -vf hwupload -c:a copy -c:v h264_nvenc -b:v 6M -f null - -vf hwupload -c:a copy -c:v h264_nvenc -b:v 5M -f null - -vf hwupload -c:a copy -c:v h264_nvenc -b:v 7M -f null -
+ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -f lavfi -i testsrc -t 50 -c:a copy -c:v h264_nvenc -b:v 1M -f null - -c:a copy -c:v h264_nvenc -b:v 2M -f null - -c:a copy -c:v h264_nvenc -b:v 3M -f null - -c:a copy -c:v h264_nvenc -b:v 4M -f null - -c:a copy -c:v h264_nvenc -b:v 5M -f null - -c:a copy -c:v h264_nvenc -b:v 6M -f null - -c:a copy -c:v h264_nvenc -b:v 7M -f null - -c:a copy -c:v h264_nvenc -b:v 8M -f null - -c:a copy -c:v h264_nvenc -b:v 9M -f null -
