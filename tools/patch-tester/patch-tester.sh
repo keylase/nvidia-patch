768cdc8e287f2546e0fe54159363f638fdea186a
Jai Luthra <me@jailuthra.in>
2025-12-11 12:56:46 +0530

    tools: patch-tester: update to 13 session

--- tools/patch-tester/patch-tester.sh
+++ tools/patch-tester/patch-tester.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-ffmpeg -y -vsync 0 -hwaccel cuda -hwaccel_output_format cuda -f lavfi -i testsrc -t 50 -c:a copy -c:v h264_nvenc -b:v 1M -f null - -c:a copy -c:v h264_nvenc -b:v 2M -f null - -c:a copy -c:v h264_nvenc -b:v 3M -f null - -c:a copy -c:v h264_nvenc -b:v 4M -f null - -c:a copy -c:v h264_nvenc -b:v 5M -f null - -c:a copy -c:v h264_nvenc -b:v 6M -f null - -c:a copy -c:v h264_nvenc -b:v 7M -f null - -c:a copy -c:v h264_nvenc -b:v 8M -f null - -c:a copy -c:v h264_nvenc -b:v 9M -f null -
+ffmpeg -y -vsync 0 \
+  -hwaccel cuda -hwaccel_output_format cuda \
+  -f lavfi -i testsrc -t 50 \
+  \
+  -c:a copy -c:v h264_nvenc -b:v 1M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 2M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 3M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 4M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 5M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 6M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 7M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 8M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 9M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 10M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 11M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 12M -f null - \
+  -c:a copy -c:v h264_nvenc -b:v 13M -f null -
--- win/tools/patch-tester/patch-tester.bat
+++ win/tools/patch-tester/patch-tester.bat
@@ -1,12 +1,16 @@
 @echo off
 
 ffmpeg.exe -y -hwaccel cuda -hwaccel_output_format cuda -f lavfi -i testsrc -t 50 ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 1M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 2M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 3M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 4M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 5M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 6M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 7M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 8M -f null - ^
--vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 9M -f null -
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 1M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 2M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 3M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 4M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 5M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 6M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 7M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 8M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 9M  -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 10M -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 11M -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 12M -f null - ^
+-vf hwupload -fps_mode passthrough -c:a copy -c:v h264_nvenc -b:v 13M -f null -
